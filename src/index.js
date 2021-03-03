3581154131b4386c4a7b77336d3837b2640a0fb5
Thomas Herrmann <mail@thoherr.de>
2021-03-03 23:36:11 +0100

    Showing the Past Moves; map history to button list

--- src/index.js
+++ src/index.js
@@ -74,6 +74,16 @@ class Game extends React.Component {
         const history = this.state.history;
         const current = history[history.length - 1]
         const winner = calculateWinner(current.squares);
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
         let status;
         if (winner) {
             status = 'Winner: ' + winner;
@@ -90,7 +100,7 @@ class Game extends React.Component {
                 </div>
                 <div className="game-info">
                     <div>{status}</div>
-                    <ol>{/* TODO */}</ol>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
